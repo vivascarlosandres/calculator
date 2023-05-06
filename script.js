54c87cfef27ce97dd0090bc6673cd7f840df8081
Carlos Vivas <65358778+cabjkiko@users.noreply.github.com>
2023-05-06 13:56:19 -0300

    Created operate function

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
     return a / b;
 }
+
+let num1;
+let num2;
+let operator;
+
+const operate = function (operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return null; //Invalid operator
+    }
+}
